1a76f6911f38d9aa42febe309c0d31f6c953e0f9
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-07-05 09:36:38 +0530

    x

---
+++ CLS/randomforest/dt-rf-DU3.R
@@ -0,0 +1,28 @@
+# Random Forests
+#https://www.r-bloggers.com/variable-importance-plot-and-variable-selection/
+
+set.seed(1)
+n=500
+library(clusterGeneration)
+library(mnormt)
+S=genPositiveDefMat("eigen",dim=15)
+S
+S=genPositiveDefMat("unifcorrmat",dim=15)
+S
+X=rmnorm(n,varcov=S$Sigma)
+X
+library(corrplot)
+corrplot(cor(X), order = "hclust")
+
+P=exp(Score)/(1+exp(Score))
+P=exp(S)/(1+exp(S))
+
+Y=rbinom(n,size=1,prob=P)
+df=data.frame(Y,X)
+allX=paste("X",1:ncol(X),sep="")
+names(df)=c("Y",allX)
+#The variable importance plot is obtained by growing some trees,
+
+require(randomForest)
+fit=randomForest(factor(Y)~., data=df)
+(VI_F=importance(fit))
