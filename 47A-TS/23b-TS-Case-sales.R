200bfd87380c1665c7aefcd8e3ac9bd4ce8691e4
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-08-07 06:48:10 +0530

    a

--- 47A-TS/23b-TS-Case-sales.R
+++ 47A-TS/23b-TS-Case-sales.R
@@ -39,3 +39,43 @@ data10
 adf.test(data10, alternative = "stationary")
 
 plot(diff(data10), ylab = "1st Order Difference of Log(Sales)")
+data11 = diff(data10)
+plot(diff(data11), ylab = "2nd Order Difference of Log(Sales)")
+data12 = diff(data11)
+
+adf.test(data12, alternative = "stationary")
+
+acf(ts(diff(diff(data))), main = "ACF Sales")
+
+pacf(ts(diff(diff(data))), main = "PACF Sales")
+
+ARIMAfit = arima(diff(data), c(2,2,0))
+summary(ARIMAfit)
+
+ARIMAautofit = auto.arima(diff(data), approximation = TRUE, trace = TRUE)
+summary(ARIMAautofit)
+
+acf(ts(diff(diff(log10(data)))), main = "ACF Log(Sales)")
+pacf(ts(diff(diff(log10(data)))), main = "PACF Log(Sales)")
+
+ARIMAautofit2 = auto.arima(diff(log10(data)), approximation = TRUE, trace = TRUE)
+summary(ARIMAautofit2)
+
+pred = predict(ARIMAautofit, n.ahead = 36)
+pred
+
+pred$pred
+pred$se
+
+plot(forecast(ARIMAautofit, h = 36))
+
+acf(ts(ARIMAautofit$residuals), main = "ACF Residual Of Sales")
+pacf(ts(ARIMAautofit$residuals), main = "PACF Residual Of Sales")
+
+autoexp_fit = ets(data10)
+summary(autoexp_fit)
+
+pred_ets = predict(autoexp_fit, h = 36)
+plot(forecast(autoexp_fit))
+summary(pred_ets)
+acf(autoexp_fit$residuals, lag.max = 20, main = "ACF Residual Of Sales")
