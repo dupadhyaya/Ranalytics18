0d7e9136e7227b4276e6ac5cfb3298c6fe8878c9
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-08-16 08:04:22 +0530

    16Aug

--- 20a-BasicGraphs/43c-corrplot.R
+++ 20a-BasicGraphs/43c-corrplot.R
@@ -1,15 +1,12 @@
 # some more advance graphs
 
-
-
-
+#install corrplot
 library(corrplot)
-res=cor(mtcars)
-res
-corrplot(res, type = "upper", order = "hclust", 
-         tl.col = "black", tl.srt = 45)
+relationship=cor(mtcars)
+relationship
+corrplot(relationship, type = "upper", order = "hclust",  tl.col = "black", tl.srt = 45)
 
-
+#install library
 library("PerformanceAnalytics")
 my_data <- mtcars[, c(1,3,4,5,6,7)]
 chart.Correlation(my_data, histogram=TRUE, pch=19)
@@ -18,3 +15,4 @@ chart.Correlation(my_data, histogram=TRUE, pch=19)
 # Get some colors
 col<- colorRampPalette(c("blue", "white", "red"))(20)
 heatmap(x = res, col = col, symm = TRUE)
+
