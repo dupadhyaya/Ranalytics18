0a6cbe9b093f83803604ad68541243b5b276d380
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-08-23 17:39:55 +0530

    23 Aug

---
+++ 19c-mtcars/12e-mtcars-summarise-dplyr.R
@@ -0,0 +1,21 @@
+#tidyverse
+
+library(dplyr)
+#Options to print increased no of rows and columns
+#options(dplyr.print_max = 1e9)
+#options(tibble.print_max = n, tibble.print_min = 3)
+#: if there are more than n rows, print only the first m rows. Use 
+options(tibble.print_max = Inf) #to always show all rows.
+options(tibble.width = Inf)
+
+#group by cyl, calculate avg hp and wt
+mtcars %>% group_by(cyl) %>% summarise(mean(hp), mean(wt))
+#group by cyl, am
+mtcars %>% group_by(cyl,am) %>% summarise(mean(hp), mean(wt))
+
+#mean and count of all columns when gp by cyl, am
+mtcars %>%group_by(cyl, am) %>% summarise_all(funs(mean, n()))
+
+#selected columns
+mtcars %>% select(cyl, am, mpg, wt, hp, qsec) %>% group_by(cyl, am) %>% summarise_all(funs(mean, max, min))
+
--- 19c-mtcars/12e-mtcars.R
+++
@@ -1,9 +0,0 @@
-#tidyverse
-
-library(dplyr)
-
-
-
-mtcars %>%group_by(cyl, am) %>% summarise_all(funs(mean, n()))
-
-aggregate(cbind(mpg, wt) ~ cyl + am, data = mtcars, FUN = plyr::each(avg = mean, n = length))
