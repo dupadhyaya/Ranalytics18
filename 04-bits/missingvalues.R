798e0d3a01338d692fc07aa0f29a9e57b4b5825c
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-09-15 14:01:05 +0530

    a

---
+++ 04-bits/missingvalues.R
@@ -0,0 +1,97 @@
+# Missing Values
+# Create, Detect, Replace, Remove, Impute, Visualise
+library(VIM)
+
+(v1 = c(1,2,NA,NA,5))
+is.na(v1)
+v1=NULL
+v1  # remove all values
+(v1 = c(1,2,NA,NA,5,6,7,NA))
+sum(is.na(v1))  # sum TRUE values ie missing values
+mean(v1)  # cannot calculate if NA values in vector
+mean(v1, na.rm=T)  # remove and calc
+(v1a = na.omit(v1))  # omit missing values and put in new vector
+v1a
+mean(v1a)
+sum(v1a) # now you can sum on non NA values
+?na.omit
+
+anyNA(v1) # is there any NA value
+anyNA(v1a)
+?anyNA
+#impute missing values of vector with mean value of non NA values
+v1
+mean(v1, na.rm=T)  # this value to be used
+v1[is.na(v1)]
+v1[is.na(v1)] = mean(v1, na.rm=T)
+v1
+
+#Use VIM package to load sleep dataset with NA values in rows & col
+library(VIM)
+data(sleep, package='VIM')
+summary(sleep)# see NA values in cols
+head(sleep)
+dim(sleep)
+complete.cases(sleep) #all complete rows without any missing values
+sum(complete.cases(sleep)) # count non NA rows/ complete 42
+sum(!complete.cases(sleep))  #count NA rows 20
+mean(complete.cases(sleep))  #42/62
+# total cases = 62, complete=42  
+sleep[complete.cases(sleep),] # list rows which are complete
+sleep[!complete.cases(sleep),] # list rows which have NA values
+
+# Particular Column
+is.na(sleep$Dream)
+sum(is.na(sleep$Dream))
+mean(is.na(sleep$Dream))  # 12/62
+
+# How many missing values
+sum(is.na(sleep))  #total in data frame
+names(sleep)# check columname
+colSums(is.na(sleep))  #NA values in each column
+rowSums(is.na(sleep))  #NA values in each row
+head(sleep)
+as.data.table(sleep)  #data.table format
+is.na(sleep[1:3,]) # first 3 rows
+head(sleep,n=3)
+
+#Tabulate
+library(mice)
+mice::md.pattern(sleep)
+#42 rows without any missing values # 2 rows with 1 NA NonD
+
+#Visualisation
+VIM::aggr(sleep, prop=F, numbers=T)
+#NonD max NA values 
+VIM::aggr(sleep, prop=T)  # Proportion
+VIM::matrixplot(sleep) # rowno and missing values in red
+VIM::marginmatrix(sleep)
+VIM::marginplot(sleep[c('Gest','Dream')], pch=c(20), col=c("darkgray", "red", "blue"))  # finding relationship between NA value
+
+
+#Exploration
+x = as.data.frame(abs(is.na(sleep)))
+head(sleep, na=5)
+head(x, n=5)
+y = x[which(apply(x, 2, sum) > 0)]
+cor(y)  #Dream - NonD 0.9
+cor(sleep, y, use="pairwise.complete.obs")
+#ignore NA, 
+#
+
+#Listwise Deletion
+options(digits=3)
+na.omit(sleep)
+cor(na.omit(sleep))
+cor(sleep, use="complete.obs")
+
+fit = lm(Dream ~ Span + Gest, data= na.omit(sleep))
+summary(fit)
+
+
+#imputations Packages - mice, Amelia, mi : mice, with, pool
+
+imp = mice::mice(sleep, seed=1234)
+?mice
+
+# explore more
---
+++ 04-bits/wordcloud1.R
@@ -0,0 +1,56 @@
+# World Cloud 2
+
+# Read the text file from file
+text <- readLines(file.choose())
+text
+
+# Load the data as a corpus
+docs = Corpus(VectorSource(text))
+docs
+
+#Text transformation
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+#Cleaning Text
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+docs <- tm_map(docs, stemDocument)
+
+
+#Document Matrix
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+#Generate Word Cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+findFreqTerms(dtm, lowfreq = 4)
+findAssocs(dtm, terms = "freedom", corlimit = 0.3)
+head(d, 10)
+
+#Plot Freq
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+       col ="lightblue", main ="Most frequent words",
+       ylab = "Word frequencies")
+
