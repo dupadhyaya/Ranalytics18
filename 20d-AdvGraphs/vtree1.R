0f18eb31fb373288de86d5ef07d32e6f2b15e44c
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-03-27 21:06:49 +0530

    a

--- 20d-AdvGraphs/vtree1.R
+++ 20d-AdvGraphs/vtree1.R
@@ -36,3 +36,52 @@ vtree(FakeData,"Severity Sex",summary="id \nid = %list% %var=Severity% %trunc=40
 
 
 grVizToPNG(g, width = NULL, height = NULL, folder = ".")
+
+
+#mtcars
+vtree(mtcars,"am")
+vtree(mtcars,"am gear")
+vtree(mtcars,"am gear cyl")
+
+vtree(mtcars,"am", horiz = F)
+#vtree(mtcars,"am", summary)
+
+vtree(mtcars,"am gear",horiz=FALSE,plain=TRUE)
+vtree(mtcars,"am gear",showlegend=TRUE,shownodelabels=FALSE)
+
+vtree(mtcars,"am gear",sameline=TRUE)
+vtree(mtcars,"am gear",sameline=TRUE,showvarnames=FALSE)
+vtree(mtcars,"am gear", splitwidth=15, lsplitwidth=8)
+
+vtree(FakeData,"Severity Sex",prune=list(Severity=c("Mild","Moderate")))
+
+vtree(FakeData,"Severity Sex",prunebelow=list(Severity=c("Mild","Moderate")))
+
+vtree(FakeData,"Severity Sex",horiz=T,labelvar=c(Severity="Severity on day 1"),labelnode=list(Sex=c(Male="M",Female="F")))
+
+vtree(FakeData,"Severity",horiz=T,  text=list(Severity=c(Mild="*Includes first-time visits*")))
+
+
+#Displaying summary statistics in nodes
+vtree(FakeData,"Severity",summary="Score %mean%",horiz=T)
+vtree(mtcars,"cyl",summary="mpg %mean%",horiz=T)
+vtree(mtcars,"cyl am",summary="mpg %mean%",horiz=T)
+
+vtree(FakeData,"Severity",horiz=FALSE,showvarnames=FALSE, summary=c( "Score \nScore: mean (SD)\n %mean% (%SD%)", "Pre \n\nPre: range\n %min%, %max%"))
+
+vtree(mtcars,"cyl",horiz=F,showvarnames=FALSE, summary=c( "mpg \n mpg: mean (SD)\n %mean% (%SD%)", "wt \n\n wt: range\n %min%, %max%"))
+
+vtree(FakeData,"Severity",summary="Viral \nViral %npct%",horiz=T,showvarnames=FALSE)
+str(FakeData)
+vtree(mtcars,"gear",summary="am \nam %npct%",horiz=FALSE,showvarnames=FALSE)
+table(mtcars$am)
+table(mtcars$am, mtcars$gear)
+#T=1, F=0:TRUE / FALSE
+
+#Venn Diagram
+vtree(FakeData,"Ind1 Ind2 Ind3 Ind4",Venn=TRUE)
+
+sequence <- with(FakeData,paste(Ind1,Ind2,Ind3,Ind4))
+sequence
+vtree(FakeData,"Ind1 Ind2 Ind3 Ind4",Venn=TRUE,seq=TRUE,
+     palette=c(Ind1=1,Ind2=2,Ind3=3,Ind4=4))
