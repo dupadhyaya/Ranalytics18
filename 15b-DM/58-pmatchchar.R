2cf76aa0454c66812aafba5e2220d23aa7e13527
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-04-26 08:21:03 +0530

    a

---
+++ 15b-DM/58-pmatchchar.R
@@ -0,0 +1,10 @@
+#partial matching
+#https://astrostatistics.psu.edu/su07/R/html/base/html/pmatch.html
+
+pmatch("m",   c("mean", "median", "mode")) # returns NA
+pmatch("med", c("mean", "median", "mode")) # returns 2
+
+pmatch(c("", "ab", "ab"), c("abc", "ab"), dup = FALSE)
+pmatch(c("", "ab", "ab"), c("abc", "ab"), dup = TRUE)
+## compare
+charmatch(c("", "ab", "ab"), c("abc", "ab"))
